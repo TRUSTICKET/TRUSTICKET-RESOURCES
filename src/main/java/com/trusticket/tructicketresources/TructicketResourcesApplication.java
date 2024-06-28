aa7d5d1f587cf9cd0dffcf05038d5f7a053e03a2
Jaehwi-So <sjh9708@gmail.com>
2024-06-28 10:10:20 +0900

    feat : 디스커버리 서버 연결

--- src/main/java/com/trusticket/tructicketresources/TructicketResourcesApplication.java
+++ src/main/java/com/trusticket/tructicketresources/TructicketResourcesApplication.java
@@ -2,8 +2,11 @@ package com.trusticket.tructicketresources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TructicketResourcesApplication {
 
 	public static void main(String[] args) {
